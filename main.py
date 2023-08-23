70bbd178c4c299c19c84ae38ac65f99563dd175e
Lukas <lukastham8@hotmail.com>
2023-08-23 23:26:42 +0800

    remove scraping web scrape code

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ app = FastAPI()
 
 app.include_router(router)
 
-# @app.on_event("startup")
-# async def startup_event():
-#     df = scrape_data()
-#     store_property_data(df)
+@app.on_event("startup")
+async def startup_event():
+    df = scrape_data()
+    store_property_data(df)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
